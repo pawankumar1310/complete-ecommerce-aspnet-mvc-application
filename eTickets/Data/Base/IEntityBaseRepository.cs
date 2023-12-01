6407c4530867bef4d2d13fd1ea98087736cdbef8
pawankumar <pawankumar131098@gmail.com>
2023-12-01 08:57:45 +0530

    Using baseRepository implmented Actor crud operation

---
+++ eTickets/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Base
+    {
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+        {
+        Task<IEnumerable<T>> GetallAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+        }
+    }
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using eTickets.Data.Base;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
 {
-    public class Actor
+    public class Actor : IEntityBase
     {
         [Key]
         public int Id { get; set; }
